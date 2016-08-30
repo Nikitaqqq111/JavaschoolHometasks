4c0c3cd93868de22dad0a0d4b65243117624e5f1
Nikitaqqq111 <nikitaevstafyev2012@yandex.ru>
2016-08-31 02:09:01 +0300

    12 задание: threads

--- src/test/java/ru/sbt/javaschool/threads/task/TaskTest.java
+++ src/test/java/ru/sbt/javaschool/threads/task/TaskTest.java
@@ -10,16 +10,15 @@ import static org.junit.Assert.*;
 public class TaskTest {
     @Test
     public void get() throws Exception {
-
         Task<Integer> task = new Task<>(() -> {
-            Thread.sleep(2000);
+            for (int i = 0; i < 1_000_000_000; i++);
             return 123;
         });
 
         Runnable run = () ->
                 System.out.println(task.get() + " " + Thread.currentThread().getName() + " " + Thread.currentThread().getPriority());
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             new Thread(run).start();
         }
     }
